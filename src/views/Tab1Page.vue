c46676c31fa5a689537034a7dfa57b17cc230a74
Ng'etich Stephen <stephenngetich5@gmail.com>
2023-11-14 14:08:55 +0300

    feat: add text input

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -13,6 +13,65 @@
       </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
+      <!-- '''' -->
+      <ion-list>
+    <ion-item>
+      <ion-input label="Title " placeholder="Mr/Mrs"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Name " placeholder="(Surname) (First Name) (Other names)"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Date of Birth " type='date'></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Home County " placeholder=""></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Postal Address " placeholder=""></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Postal code" placeholder=""></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Town/City " placeholder=""></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Telephone Number" type="tel" placeholder="888-888-8888"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Mobile Number" type="tel" placeholder="0711-XXXX-XXXX"></ion-input>
+    </ion-item>
+
+    <ion-item>
+      <ion-input label="Email Address" type="email" placeholder="email@domain.com"></ion-input>
+    </ion-item>
+
+    <ion-item>
+      <ion-input label="Telephone input" type="tel" placeholder="888-888-8888"></ion-input>
+    </ion-item>
+      <ion-item>
+      <ion-input label="Name of Alternative Contact" placeholder="(Surname) (First Name) (Other names)"></ion-input>
+    </ion-item>
+
+    <ion-item>
+      <ion-input label="Telephone No" type="tel" placeholder="888-888-8888"></ion-input>
+    </ion-item>
+
+     <ion-item>
+      <ion-input label="Are you living with disability" placeholder=""></ion-input>
+    </ion-item>
+     <ion-item>
+      <ion-input label="Nature of Disability" placeholder=""></ion-input>
+    </ion-item>
+     <ion-item>
+      <ion-input label="NCPD Reg No" placeholder=""></ion-input>
+    </ion-item>
+     <ion-item>
+      <ion-input label="NCPD Date of Registration" placeholder=""></ion-input>
+    </ion-item>
+  </ion-list>
+      <!-- .... -->
     </ion-content>
   </ion-page>
 </template>
